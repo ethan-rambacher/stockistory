27a8a098ccef78e0ca38b62e5989128c68dfe7c1
Ethan Rambacher <eerambach@gmail.com>
2022-01-02 22:19:23 -0500

    Pull PortfolioChart into new component - in progress

---
+++ src/app/PortfolioChart/PortfolioChart.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Chart, ChartAxis, ChartGroup, ChartLine, ChartVoronoiContainer } from '@patternfly/react-charts';
+
+type PortfolioData = {
+    chartData: string
+}
+
+const PortfolioChart: React.FunctionComponent = ({ chartData }: PortfolioData) => (
+    <Chart
+        ariaDesc="Average number of pets"
+        ariaTitle="Line chart example"
+        containerComponent={<ChartVoronoiContainer labels={({ datum }) => `${datum.name}: ${datum.y}`} constrainToVisibleArea />}
+        legendData={[{ name: 'Cats' }, { name: 'Dogs', symbol: { type: 'dash' } }, { name: 'Birds' }, { name: 'Mice' }]}
+        legendOrientation="vertical"
+        legendPosition="right"
+        height={250}
+        maxDomain={{ y: 10 }}
+        minDomain={{ y: 0 }}
+        padding={{
+            bottom: 50,
+            left: 50,
+            right: 200, // Adjusted to accommodate legend
+            top: 50
+        }}
+        width={600}
+    >
+        <ChartAxis tickValues={[2, 3, 4]} />
+        <ChartAxis dependentAxis showGrid tickValues={[2, 5, 8]} />
+        <ChartGroup>
+            <ChartLine
+                data={ [
+                    { name: chartData, x: '2015', y: 1},
+                    { name: chartData, x: '2016', y: 2},
+                    { name: chartData, x: '2017', y: 5},
+                    { name: chartData, x: '2018', y: 4}
+                ] }
+            />
+            <ChartLine
+                data={[
+                    { name: 'Dogs', x: '2015', y: 2 },
+                    { name: 'Dogs', x: '2016', y: 1 },
+                    { name: 'Dogs', x: '2017', y: 7 },
+                    { name: 'Dogs', x: '2018', y: 4 }
+                ]}
+                style={{
+                    data: {
+                        strokeDasharray: '3,3'
+                    }
+                }}
+            />
+        </ChartGroup>
+    </Chart>
+)
+
+export { PortfolioChart };
